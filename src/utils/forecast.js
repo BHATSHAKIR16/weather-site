f2de2d626273ec54d745fbc1c47f36484d18035d
shakir <bhatshakir16@gmail.com>
2021-11-15 22:52:09 +0530

    add additional weather info

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -7,7 +7,7 @@ const forecast = (latitude,longitude,callback)=>{
         }else if(response.body.error){
             callback("location not found",undefined)
         }else{
-            callback(undefined,"it is currently " + response.body.current.temperature + " degrees out. it feels like " + response.body.current.feelslike + " degrees out")
+            callback(undefined, response.body.current.weather_descriptions[0] + ". it is currently " + response.body.current.temperature + " degrees out. it feels like " + response.body.current.feelslike + " degrees out. The humidity is " + response.body.current.humidity + "%. ")
               
             }
         
